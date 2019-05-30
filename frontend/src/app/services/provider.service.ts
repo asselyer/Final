f9c2271dac2721215a6f729e9e8e9efb3f7bffb1
asselyer <asel.yer98@gmail.com>
2019-05-31 05:07:11 +0600

    Final Exam Assel Yernazarova

---
+++ frontend/src/app/services/provider.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { MainService } from './main.service';
+import {HttpClient} from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProviderService extends MainService {
+
+  public sendMessage = new EventEmitter<string>();
+
+  constructor(http: HttpClient) {
+    super(http);
+  }
+}
+
+}
